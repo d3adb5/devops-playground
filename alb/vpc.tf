b5aa02dd294bb5ca3fb315e261e47f4e4c4e0501
d3adb5 <me@d3adb5.net>
2023-02-03 10:37:15 -0800

    fix(alb): use proper data source for managed prefix list
    Use the proper data source (aws_ec2_managed_prefix_list) for the managed
    prefix list containing IP addresses used by CloudFront.

--- alb/vpc.tf
+++ alb/vpc.tf
@@ -1,4 +1,4 @@
-data "aws_prefix_list" "cloudfront" {
+data "aws_ec2_managed_prefix_list" "cloudfront" {
   name = "com.amazonaws.global.cloudfront.origin-facing"
 }
 
@@ -11,6 +11,13 @@ resource "aws_security_group" "default" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = data.aws_prefix_list.cloudfront.cidr_blocks
+    cidr_blocks = data.aws_ec2_managed_prefix_list.cloudfront.entries.*.cidr
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
