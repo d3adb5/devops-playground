453a2ad9b386147bcb1e0f50f2cb11ab249f003b
d3adb5 <me@d3adb5.net>
2023-10-12 04:58:18 -0700

    feat: create github actions oidc provider on aws
    Create an OIDC provider on AWS for GitHub Actions as well as a role that
    can read the Terraform state.

--- terraform/aws-s3-private-backup/iam.tf
+++ terraform/aws-s3-private-backup/iam.tf
@@ -27,3 +27,86 @@ data "aws_iam_policy_document" "bucket_access" {
     effect    = "Allow"
   }
 }
+
+#######################################################################
+#             OpenID Connect Provider for GitHub Actions              #
+#######################################################################
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  count           = terraform.workspace == "default" ? 1 : 0
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["token.actions.githubusercontent.com"]
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
+}
+
+resource "aws_iam_role" "tfstate_readonly_access" {
+  count              = terraform.workspace == "default" ? 1 : 0
+  name               = "PrivateS3BackupTfstateROAccess"
+  assume_role_policy = data.aws_iam_policy_document.oidc_trust[0].json
+}
+
+resource "aws_iam_role_policy" "state_readonly_access" {
+  count  = terraform.workspace == "default" ? 1 : 0
+  name   = "StateReadonlyAccess"
+  role   = aws_iam_role.tfstate_readonly_access[0].name
+  policy = data.aws_iam_policy_document.state_readonly_access[0].json
+}
+
+data "aws_iam_policy_document" "oidc_trust" {
+  count = terraform.workspace == "default" ? 1 : 0
+
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions[0].arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${aws_iam_openid_connect_provider.github_actions[0].url}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "${aws_iam_openid_connect_provider.github_actions[0].url}:sub"
+      values   = ["repo:d3adb5/devops-playground:*"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+data "aws_iam_policy_document" "state_readonly_access" {
+  count = terraform.workspace == "default" ? 1 : 0
+
+  statement {
+    sid       = "AllowListingObjects"
+    actions   = ["s3:ListBucket"]
+    resources = [module.tfstate_backend[0].s3_bucket_arn]
+    effect    = "Allow"
+  }
+
+  statement {
+    sid       = "AllowReadOnBucket"
+    actions   = ["s3:GetObject"]
+    resources = ["${module.tfstate_backend[0].s3_bucket_arn}/*"]
+    effect    = "Allow"
+  }
+
+  statement {
+    sid = "AllowReadWriteOnDynamoDBTable"
+
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:GetItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:UpdateItem"
+    ]
+
+    resources = [module.tfstate_backend[0].dynamodb_table_arn]
+    effect    = "Allow"
+  }
+}
